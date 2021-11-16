143f3e39caff93337fa2e76a647164e6b3d7c01d
ViliAu <36374215+ViliAu@users.noreply.github.com>
2021-11-16 23:10:54 +0200

    Trying to resolve .log files

--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInventory : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class PlayerInventory : MonoBehaviour {
+    
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
