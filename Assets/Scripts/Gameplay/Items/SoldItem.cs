0fb1ef241125b0c050ed132b7ca0dc7deca450d8
Kakkatykki <36374215+ViliAu@users.noreply.github.com>
2021-11-29 09:36:12 +0200

    Added item buying

---
+++ Assets/Scripts/Gameplay/Items/SoldItem.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class SoldItem : Interactable {
+    [SerializeField] private Item itemToSell = null;
+    [SerializeField] private float restockTime = 30f;
+    
+    [Server]
+    public override void OnServerInteract(NetworkIdentity client) {
+        base.OnServerInteract(client);
+        PlayerInventory inv = client.GetComponent<PlayerInventory>();
+        if (EntityManager.LocalPlayer.Player_Wallet.Balance < itemToSell.Price) {
+            return;
+        }
+        GameObject g = Instantiate(itemToSell.gameObject, transform.position, Quaternion.identity);
+        NetworkServer.Spawn(g);
+        bool success = inv.AddItem(client, g.GetComponent<Item>());
+        if (success) {
+            RpcSetEntityActive(g, false);
+        }
+        else {
+            RpcThrowItem(g);
+        }
+        RpcSetEntityActive(gameObject, false);
+        Invoke("ActivateSoldItem", restockTime);
+        PlayFX();
+    }
+
+    [Server]
+    private void ActivateSoldItem() {
+        RpcSetEntityActive(gameObject, true);
+    }
+
+    [ClientRpc]
+    private void RpcThrowItem(GameObject g) {
+        g.GetComponent<Rigidbody>().AddForce(Vector3.up + transform.forward, ForceMode.Impulse);
+    }
+
+    [ClientRpc]
+    private void PlayFX() {
+        SoundSystem.PlaySound("transaction_success", transform.position);
+    }
+}
